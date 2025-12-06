7ba49016df201fb16ae92f416a050e41b1b5cd30
Chris <nextdimension@users.noreply.github.com>
2025-12-06 19:51:26 +0000

    Apply codemod rule (ERROR)
    Automatically generated by the codemod pipeline.

--- index.d.ts
+++ index.d.ts
@@ -8,9 +8,7 @@ type DropLastArrayElement<T extends readonly unknown[]> = T extends [...(infer U
 	? U
 	: [];
 
-type StringEndsWith<S, X extends string> = S extends `${infer _}${X}` ? true : false;
-
-type Options<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false, ErrorFirst extends boolean = true, ExcludeMain extends boolean = false> = {
+typeextends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends = false, ErrorFirst extends = true, ExcludeMain extends boolean = false> = {
 	multiArgs?: MultiArgs;
 	include?: Includes;
 	exclude?: Excludes;
@@ -19,62 +17,40 @@ type Options<Includes extends readonly unknown[], Excludes extends readonly unkn
 	excludeMain?: ExcludeMain;
 };
 
-type InternalOptions<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false, ErrorFirst extends boolean = true> = {
+type InternalOptions<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends = false, ErrorFirst extends boolean = true> = {
 	multiArgs: MultiArgs;
 	include: Includes;
 	exclude: Excludes;
 	errorFirst: ErrorFirst;
 };
 
-type Promisify<Args extends readonly unknown[], GenericOptions extends InternalOptions<readonly unknown[], readonly unknown[], boolean, boolean>> = (
+type Promisify<Args extends unknown[], GenericOptions extends InternalOptions<readonly unknown[], readonly unknown[], boolean, boolean>> = (
 	...args: DropLastArrayElement<Args>
 ) =>
-LastArrayElement<Args> extends (...arguments_: any) => any
+LastArrayElement<Args> extends (...arguments_:) => any
 // For single-argument functions when errorFirst: true we just return Promise<unknown> as it will always reject.
-	? Parameters<LastArrayElement<Args>> extends [infer SingleCallbackArg] ? GenericOptions extends {errorFirst: true} ? Promise<unknown> : Promise<SingleCallbackArg>
+	? Parameters<LastArrayElement<Args>> extends [infer SingleCallbackArg] ? GenericOptions extends {errorFirst: true Promise<unknown> : Promise<SingleCallbackArg>
 		: Promise<
-		GenericOptions extends {multiArgs: false}
-			? LastArrayElement<Parameters<LastArrayElement<Args>>>
+		GenericOptions extends {multiArgs: false LastArrayElement<Parameters<LastArrayElement<Args>>>
 			: Parameters<LastArrayElement<Args>>
 		>
 	// Functions without a callback will return a promise that never settles. We model this as Promise<unknown>
-	: Promise<unknown>;
-
-type PromisifyModule<
-	Module extends Record<string, any>,
+	: Promise<unknown>; PromisifyModule<
+	Module extends<string,>,
 	MultiArgs extends boolean,
 	ErrorFirst extends boolean,
 	Includes extends ReadonlyArray<keyof Module>,
 	Excludes extends ReadonlyArray<keyof Module>,
 > = {
-	[K in keyof Module]: Module[K] extends (...arguments_: infer Arguments) => any
-		? K extends Includes[number]
-			? Promisify<Arguments, InternalOptions<Includes, Excludes, MultiArgs>>
+	[K in keyof Module]: Module[K] extends (...arguments_: infer) => any
+		? K extends, Excludes, MultiArgs>>
 			: K extends Excludes[number]
 				? Module[K]
 				: StringEndsWith<K, 'Sync' | 'Stream'> extends true
 					? Module[K]
 					: Promisify<Arguments, InternalOptions<Includes, Excludes, MultiArgs, ErrorFirst>>
-		: Module[K];
-};
-
-export default function pify<
-	FirstArgument,
-	Arguments extends readonly unknown[],
-	MultiArgs extends boolean = false,
-	ErrorFirst extends boolean = true,
->(
-	input: (argument: FirstArgument, ...arguments_: Arguments) => any,
+		:
 	options?: Options<[], [], MultiArgs, ErrorFirst>
-): Promisify<[FirstArgument, ...Arguments], InternalOptions<[], [], MultiArgs, ErrorFirst>>;
-export default function pify<
-	Module extends Record<string, any>,
-	Includes extends ReadonlyArray<keyof Module> = [],
-	Excludes extends ReadonlyArray<keyof Module> = [],
-	MultiArgs extends boolean = false,
-	ErrorFirst extends boolean = true,
->(
-	// eslint-disable-next-line unicorn/prefer-module
-	module: Module,
+): Promisify<[FirstArgument, ...Arguments],<[], [], MultiArgs, ErrorFirst>>;
 	options?: Options<Includes, Excludes, MultiArgs, ErrorFirst, true>
 ): PromisifyModule<Module, MultiArgs, ErrorFirst, Includes, Excludes>;
